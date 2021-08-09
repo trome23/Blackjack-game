cd2b89ff3e0fa728315f05bcc16cb6c325676c2d
Tyson Romero <romennoodles23@yahoo.com>
2021-08-09 15:34:54 -0600

    moved card array and 1st/2nd + cards into startGame() function

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
 
-let firstCard = getRandomCard()
-let secondCard = getRandomCard()
-let cards = [firstCard, secondCard]
-let sum = firstCard + secondCard;
+let cards = []
+let sum = 0
 let hasBlackJack = false
-let isAlive = true
+let isAlive = false
 let message = ""
 
 let messageEl = document.getElementById("message-el")
@@ -12,12 +10,23 @@ let messageEl = document.getElementById("message-el")
 let sumEl = document.querySelector("#sum-el")
 let cardsEl = document.querySelector("#cards-el")
 
-function getRandomCard() {
-    let randomCard = Math.floor(Math.random() * 13) + 1
-    return randomCard
+function getRandomCard() {    
+    let randomNumber = Math.floor(Math.random() * 13) + 1
+    if (randomNumber > 10) {
+        return 10
+    } else if (randomNumber === 1) {
+        return 11
+    } else {
+        return randomNumber
+    }
 }
 
 function startGame() {
+    isAlive = true
+    let firstCard = getRandomCard()
+    let secondCard = getRandomCard()
+    cards = [firstCard,secondCard]
+    sum = firstCard + secondCard
     renderGame()
 }
 
