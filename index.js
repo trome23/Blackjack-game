de7c57c913fb765422cace1e1ea284be36e1ed4e
Tyson Romero <romennoodles23@yahoo.com>
2021-08-08 20:05:39 -0600

    showing results in the DOM

--- index.js
+++ index.js
@@ -1,18 +1,27 @@
 
 let firstCard = 10;
-let secondCard = 12;
+let secondCard = 7;
 let sum = firstCard + secondCard;
 let hasBlackJack = false
 let isAlive = true
+let message = ""
 
-if (sum <= 20) {
-    message = "Do you want to draw a new card? 🙂"
-} else if(sum === 21) {
-    message = "Oh yeah! You got Blackjack! 🥳"
-    hasBlackJack = true
-} else {
-    message = "You're out of the game! 😭"
-    isAlive = false
+let messageEl = document.getElementById("message-el")
+// let sumEl = document.getElementById("sum-el") <--another way to do queryselector
+let sumEl = document.querySelector("#sum-el")
+let cardsEl = document.querySelector("#cards-el")
+
+function deal() {
+    sumEl.textContent = "Sum: " + sum
+    cardsEl.textContent = "Your Hand: " + firstCard + " - " + secondCard
+    if (sum <= 20) {
+        message = "Do you want to draw a new card?"
+    } else if(sum === 21) {
+        message = "Oh yeah! You got Blackjack!"
+        hasBlackJack = true
+    } else {
+        message = "You're out of the game!"
+        isAlive = false
+    }
+    messageEl.textContent = message
 }
-
-console.log(message)
